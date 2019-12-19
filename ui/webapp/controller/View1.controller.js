d74882b0844f29435142ce11c318028a585488d5
Maxi Streifeneder <maximilian.streifeneder@sap.com>
2019-12-19 15:12:58 +0100

    fix grunt issues, move js-files to controller

--- ui/webapp/controller/View1.controller.js
+++ ui/webapp/controller/View1.controller.js
@@ -1,11 +1,21 @@
 /* global msalconfig, Msal */
-sap.ui.define(["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/model/json/JSONModel"],
-	function (Controller, MessageToast, JSONModel) {
+sap.ui.define(["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/model/json/JSONModel", "con/sap/ui/libs/msal"],
+	function (Controller, MessageToast, JSONModel, msal) {
 		"use strict";
 
 		return Controller.extend("con.sap.ui.controller.View1", {
+
+			msalconfig: {
+				clientID: "59140526-7c63-4d3c-b507-cfe065ef2f99",
+				redirectUri: location.origin + "/ui/",
+				graphBaseEndpoint: "https://graph.microsoft.com/v1.0/",
+				userInfoSuffix: "me/",
+				queryMessagesSuffix: "me/messages?$search=\"$1\"&$top=150",
+				graphAPIScopes: ["User.Read", "Mail.Read"]
+			},
+
 			onInit: function () {
-				this.oUserAgentApplication = new Msal.UserAgentApplication(msalconfig.clientID, null,
+				this.oUserAgentApplication = new Msal.UserAgentApplication(this.msalconfig.clientID, null,
 					function (errorDesc, token, error, tokenType) {
 						if (errorDesc) {
 							var formattedError = JSON.stringify(error, null, 4);
@@ -19,11 +29,11 @@ sap.ui.define(["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/model
 							this.fetchUserInfo();
 						}
 					}.bind(this), {
-						redirectUri: msalconfig.redirectUri
-					});
+					redirectUri: this.msalconfig.redirectUri
+				});
 				//Previous version of msal uses redirect url via a property
 				if (this.oUserAgentApplication.redirectUri) {
-					this.oUserAgentApplication.redirectUri = msalconfig.redirectUri;
+					this.oUserAgentApplication.redirectUri = this.msalconfig.redirectUri;
 				}
 				// If page is refreshed, continue to display user info
 				if (!this.oUserAgentApplication.isCallback(window.location.hash) && window.parent === window && !window.opener) {
@@ -34,8 +44,8 @@ sap.ui.define(["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/model
 				}
 			},
 			onSwitchSession: function (oEvent) {
-				var oSessionModel = oEvent.getSource().getModel('session');
-				var bIsLoggedIn = oSessionModel.getProperty('/displayName');
+				var oSessionModel = oEvent.getSource().getModel("session");
+				var bIsLoggedIn = oSessionModel.getProperty("/displayName");
 				if (bIsLoggedIn) {
 					this.oUserAgentApplication.logout();
 					return;
@@ -44,67 +54,63 @@ sap.ui.define(["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/model
 			},
 
 			fetchUserInfo: function () {
-				this.callGraphApi(msalconfig.graphBaseEndpoint + msalconfig.userInfoSuffix, function (response) {
+				this.callGraphApi(this.msalconfig.graphBaseEndpoint + this.msalconfig.userInfoSuffix, function (response) {
 					$.sap.log.info("Logged in successfully!", response);
-					this.getView().getModel('session').setData(response);
+					this.getView().getModel("session").setData(response);
 				}.bind(this));
 			},
 			callGraphApi: function (sEndpoint, fnCb) {
 				var user = this.oUserAgentApplication.getUser();
 				if (!user) {
-					this.oUserAgentApplication.loginRedirect(msalconfig.graphAPIScopes);
+					this.oUserAgentApplication.loginRedirect(this.msalconfig.graphAPIScopes);
 				} else {
-					this.oUserAgentApplication.acquireTokenSilent(msalconfig.graphAPIScopes)
+					this.oUserAgentApplication.acquireTokenSilent(this.msalconfig.graphAPIScopes)
 						.then(function (token) {
-								$.ajax({
-										url: sEndpoint,
-										type: "GET",
-										beforeSend: function (xhr) {
-											xhr.setRequestHeader("Authorization", "Bearer " + token);
-										}
-									})
-									.then(fnCb)
-									.fail(function (error) {
-										MessageToast.show("Error, please check the log for details");
-										$.sap.log.error(JSON.stringify(error.responseJSON.error));
-									});
-							}.bind(this),
-							function (error) {
-								if (error) {
-									this.oUserAgentApplication.acquireTokenRedirect(msalconfig.graphAPIScopes);
+							$.ajax({
+								url: sEndpoint,
+								type: "GET",
+								beforeSend: function (xhr) {
+									xhr.setRequestHeader("Authorization", "Bearer " + token);
 								}
-							}.bind(this));
+							})
+								.then(fnCb)
+								.fail(function (error) {
+									MessageToast.show("Error, please check the log for details");
+									$.sap.log.error(JSON.stringify(error.responseJSON.error));
+								});
+
+						});
 				}
 			},
 
 			onPressLink: function (oEvent) {
 				var sLinkText = oEvent.getSource().getText();
 				var oApp = this.getView().getContent()[0];
-				this.callGraphApi(msalconfig.graphBaseEndpoint + msalconfig.queryMessagesSuffix.replace("$1", sLinkText), function (results) {
+				this.callGraphApi(this.msalconfig.graphBaseEndpoint + this.msalconfig.queryMessagesSuffix.replace("$1", sLinkText), function (results) {
 					results.value = results.value.map(function (o) {
-						o.bodyPreview = o.bodyPreview.replace(sLinkText, '<strong>' + sLinkText + '</strong>');
+						o.bodyPreview = o.bodyPreview.replace(sLinkText, "<strong>" + sLinkText + "</strong>");
 						return o;
 					});
-					var oResultsPage = oApp.getPages()[2].setModel(new JSONModel(results), 'msData');
+					var oResultsPage = oApp.getPages()[2].setModel(new JSONModel(results), "msData");
 					oApp.to(oResultsPage.getId());
 				});
 			},
 
 			onOpenEmail: function (oEvent) {
-				var sEmail = oEvent.getSource().getBindingContext('msData').getProperty('webLink');
-				window.open(sEmail, '_blank');
+				var sEmail = oEvent.getSource().getBindingContext("msData").getProperty("webLink");
+				window.open(sEmail, "_blank");
 			},
 			onProductClick: function (oEvent) {
-				var oApp = this.getView().getContent()[0];
+				var oApp = this.getView().byId("idAppControl");
 				var sBindingPath = oEvent.getSource().getBindingContext().getPath();
 				var oDetailsPage = oApp.getPages()[1].bindElement(sBindingPath);
 				oApp.to(oDetailsPage.getId());
 			},
 
 			onNavButtonPress: function (oEvent) {
-				var oApp = this.getView().getContent()[0];
+				var oApp = this.getView().byId("idAppControl");
 				var oStartPage = oApp.getPages()[0];
 				oApp.back(oStartPage.getId());
-			},
+			}
 		});
 	});
